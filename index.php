d036bac64eb5debdb5fe8f45403a16b667b0138e
ranvier11 <ranvier11@gmail.com>
2018-02-11 11:39:13 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css\bootstrap.min.css">    
+    <title>Booker</title>
+</head>
+<body>
+    <div id="booking">
+        <h2>Rejestracja</h2>
+    </div>
+    <div id="login">
+        <h2>Zaloguj</h2>
+    </div>
+
+</body>
+</html>
